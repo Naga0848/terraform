3dbe91315ea361f34481613b1e691d21b66f37ea
Nagashankar <nagashankar0848@gmail.com>
2026-01-07 15:15:36 +0530

    terraform

---
+++ ec2/main.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic and all outbound traffic"
+  
+
+  tags = {
+    Name = "allow_all"
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+}
+
+}
